9698b398ddb8b6cb76540f436af6661ede1e2272
cutieangel34 <59465809+cutieangel34@users.noreply.github.com>
2020-09-16 22:59:21 +0800

    gg

---
+++ TestPatternMovement/Project/Enemy1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project
+{
+    class Enemy1:GameObject
+    {
+        public override void Initialize()
+        {
+            //initialize all the variables
+            health = 1;
+            speed = 100.0f;
+            fireTime = 0.0f;
+            fireRate = 500.0f;
+            orientation = 0f;
+            texture = Game1.assets["enemy1"];
+            position = new Vector2(0, 0);
+            origin = new Vector2(texture.Width / 2.0f, texture.Height / 2.0f); 
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            KinematicSeek(gameTime);
+            LineOfSight(gameTime);
+        }
+
+        //Reference from notes Lecture 3
+        public void KinematicSeek(GameTime gameTime)
+        {
+            Vector2 velocity = Game1.player.position - position;
+            velocity.Normalize();
+            velocity *= speed;
+            position += velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            Orientation(velocity);
+        }
+
+        public void LineOfSight(GameTime gameTime)
+        {
+            if (gameTime.TotalGameTime.TotalMilliseconds > fireTime)
+            {
+                fireTime = (float)gameTime.TotalGameTime.TotalMilliseconds + fireRate;
+                
+                if (InLOS(90, 300, Game1.player.position, position, orientation))
+                {
+                    EnemyBullet tempBullet = new EnemyBullet();
+                    tempBullet.setOwner(this);
+                    tempBullet.Initialize();
+                    Game1.enemyBulletList.Add(tempBullet);
+                }              
+            }
+        }
+       
+        public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            spriteBatch.Draw(texture, position, null, Color.White, orientation, origin, 1.0f, SpriteEffects.None, 0.99f);
+        }
+
+        //Reference from notes Lecture 3, part of kinematic seek
+        public void Orientation(Vector2 velocity)
+        {
+            //https://stackoverflow.com/questions/2276855/xna-2d-vector-angles-whats-the-correct-way-to-calculate
+            orientation = (float)Math.Atan2(velocity.Y, velocity.X);
+        }
+    }
+}
---
+++ TestPatternMovement/Project/EnemyBullet.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project
+{
+    public class EnemyBullet : GameObject
+    {
+        private GameObject owner;
+        private Vector2 direction;
+
+        public override void Initialize()
+        {
+            //initialize all the variables
+            speed = 300.0f;
+            name = "enemyBullet";           
+            texture = Game1.assets["enemyBullet"];           
+            origin = new Vector2(texture.Width / 2.0f, texture.Height / 2.0f);
+            if (owner != null)
+            {
+                orientation = owner.orientation;
+                position = owner.position;
+            }
+            //direction = Game1.player.position + new Vector2 (Game1.screenWidth, Game1.screenHeight);         
+            direction = Game1.player.position - position;
+        }
+
+        public void setOwner(GameObject owner)
+        {
+            this.owner = owner;
+        }
+       
+        public override void Update(GameTime gameTime)
+        {
+            //Vector2 velocity = direction - position;
+            Vector2 velocity = direction;
+            velocity.Normalize();
+            velocity *= speed;
+            position += velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;          
+
+            if (position.X > Game1.screenWidth || position.X < 0 || position.Y > Game1.screenHeight)
+            {
+                Game1.enemyBulletList.Remove(this);
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            spriteBatch.Draw(texture, position, null, Color.White, orientation, origin, 1.0f, SpriteEffects.None, 1.0f);
+        }
+
+    }
+  
+}
