90df3c56b39f15cd0cf7aa3011c8abbdbc101849
cutieangel34 <59465809+cutieangel34@users.noreply.github.com>
2020-10-12 13:05:55 +0800

    updated

--- Program/Project/Game1.cs
+++ Program/Project/Game1.cs
@@ -101,7 +101,7 @@ namespace Project
             lastPress = 0f;
             pauseCounter = 0;
             distance = 0f;
-            currentLevel = 1;
+            currentLevel = 2;
             tutorialStep = 1;
             bossOut = false;
             missileCollision = false;
